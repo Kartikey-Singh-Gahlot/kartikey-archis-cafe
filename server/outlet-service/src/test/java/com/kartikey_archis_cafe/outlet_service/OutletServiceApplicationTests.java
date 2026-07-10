d5ad853826e02efa258ae1c1097a471951e5a817
Kartikey Singh Gahlot <kartikeysinghgahlot@Kartikeys-MacBook-Air.local>
2026-07-10 16:46:58 +0530

    added basic microservices for backend

---
+++ server/outlet-service/src/test/java/com/kartikey_archis_cafe/outlet_service/OutletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartikey_archis_cafe.outlet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
